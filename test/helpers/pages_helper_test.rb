6f4eb97243e7a3af844e528cc2b611472f979086
adamlingo <adamlingo@yahoo.com>
2016-04-08 17:01:33 -0500

    Kill auto-flunk

--- test/helpers/pages_helper_test.rb
+++ test/helpers/pages_helper_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class PagesHelperTest < ActionView::TestCase
   def test_sanity
-    flunk "Need real tests"
+    # flunk "Need real tests"
+    # Kill flunk, add another test later if needed
   end
 end
