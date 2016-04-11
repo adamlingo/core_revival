9ec603436822598a7a1a34631207f2d0f61092da
adamlingo <adamlingo@yahoo.com>
2016-04-11 12:41:48 -0500

    use helper to fix browser tab/page heading

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
-end
+
+  # Return full title on each page
+  def full_title(page_title = '')
+    base_title = "Core Employee Solutions"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+end 
