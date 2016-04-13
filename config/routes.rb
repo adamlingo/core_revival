2ef0d141332af78363c4e4e5c1aaf229832905a4
adamlingo <adamlingo@yahoo.com>
2016-04-13 14:18:22 -0500

    Database/config reflects utility of ActiveAdmin

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,33 @@ Rails.application.routes.draw do
 
   get  'companies' => 'pages#companies'
 
+  # Devise routes with ActiveAdmin
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  #      new_admin_user_session GET        /admin/login(.:format)                    active_admin/devise/sessions#new
+  #          admin_user_session POST       /admin/login(.:format)                    active_admin/devise/sessions#create
+  #  destroy_admin_user_session DELETE|GET /admin/logout(.:format)                   active_admin/devise/sessions#destroy
+  #         admin_user_password POST       /admin/password(.:format)                 active_admin/devise/passwords#create
+  #     new_admin_user_password GET        /admin/password/new(.:format)             active_admin/devise/passwords#new
+  #    edit_admin_user_password GET        /admin/password/edit(.:format)            active_admin/devise/passwords#edit
+  #                             PATCH      /admin/password(.:format)                 active_admin/devise/passwords#update
+  #                             PUT        /admin/password(.:format)                 active_admin/devise/passwords#update
+  #                  admin_root GET        /admin(.:format)                          admin/dashboard#index
+  # batch_action_admin_admin_users POST       /admin/admin_users/batch_action(.:format) admin/admin_users#batch_action
+  #           admin_admin_users GET        /admin/admin_users(.:format)              admin/admin_users#index
+  #                             POST       /admin/admin_users(.:format)              admin/admin_users#create
+  #        new_admin_admin_user GET        /admin/admin_users/new(.:format)          admin/admin_users#new
+  #       edit_admin_admin_user GET        /admin/admin_users/:id/edit(.:format)     admin/admin_users#edit
+  #            admin_admin_user GET        /admin/admin_users/:id(.:format)          admin/admin_users#show
+  #                             PATCH      /admin/admin_users/:id(.:format)          admin/admin_users#update
+  #                             PUT        /admin/admin_users/:id(.:format)          admin/admin_users#update
+  #                             DELETE     /admin/admin_users/:id(.:format)          admin/admin_users#destroy
+  #             admin_dashboard GET        /admin/dashboard(.:format)                admin/dashboard#index
+  #              admin_comments GET        /admin/comments(.:format)                 admin/comments#index
+  #                             POST       /admin/comments(.:format)                 admin/comments#create
+  #               admin_comment GET        /admin/comments/:id(.:format)             admin/comments#show
+  #                             DELETE     /admin/comments/:id(.:format)             admin/comments#destroy
+
   # Devise user routes
   devise_for :users
   # All included routes for Devise (rake routes):
@@ -26,7 +53,8 @@ Rails.application.routes.draw do
 
 
   # make Employees editable records that belong to companies by id
-  
+  # resources :companies do
+  #   resources :employees
   # this creates resources to have EEs belong to companies, and are listed here:
   #     company_employees GET    /companies/:company_id/employees(.:format)          employees#index
   #                       POST   /companies/:company_id/employees(.:format)          employees#create
@@ -47,6 +75,8 @@ Rails.application.routes.draw do
   #                       GET    /:controller(/:action(/:id(.:format)))              :controller#:action
 
 
+
+  # DEFAULT DOCUMENTATION OF ROUTE-HANDLING WITH REGULAR/NESTED RESOURCES
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
