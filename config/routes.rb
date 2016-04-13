77df4a75a0bc4db081aebc63227e324d21d4e0e4
adamlingo <adamlingo@yahoo.com>
2016-04-12 22:09:57 -0500

    New db routes for Devise users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,28 @@
 Rails.application.routes.draw do
+  
   # home page is root of applicaiton
   root                'pages#home'
 
   get  'companies' => 'pages#companies'
+
+  # Devise user routes
+  devise_for :users
+  # All included routes for Devise (rake routes):
+  #      new_user_session GET    /users/sign_in(.:format)               devise/sessions#new
+  #          user_session POST   /users/sign_in(.:format)               devise/sessions#create
+  #  destroy_user_session DELETE /users/sign_out(.:format)              devise/sessions#destroy
+  #         user_password POST   /users/password(.:format)              devise/passwords#create
+  #     new_user_password GET    /users/password/new(.:format)          devise/passwords#new
+  #    edit_user_password GET    /users/password/edit(.:format)         devise/passwords#edit
+  #                       PATCH  /users/password(.:format)              devise/passwords#update
+  #                       PUT    /users/password(.:format)              devise/passwords#update
+  # cancel_user_registration GET    /users/cancel(.:format)             devise/registrations#cancel
+  #     user_registration POST   /users(.:format)                       devise/registrations#create
+  # new_user_registration GET    /users/sign_up(.:format)               devise/registrations#new
+  #edit_user_registration GET    /users/edit(.:format)                  devise/registrations#edit
+  #                       PATCH  /users(.:format)                       devise/registrations#update
+  #                       PUT    /users(.:format)                       devise/registrations#update
+  #                       DELETE /users(.:format)                       devise/registrations#destroy
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
