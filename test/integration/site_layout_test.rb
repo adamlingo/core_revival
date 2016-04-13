af5aa1fd5fb17c6e9ccb0550becce833bbf56ef9
adamlingo <adamlingo@yahoo.com>
2016-04-13 12:13:35 -0500

    test tweak

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,13 +1,17 @@
 require "test_helper"
+# use app/test/test_helper.rb
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  def test_sanity
-    # flunk "Need real tests"
+  
+  # tests that all links are valid/used in layouts/application.html.erb
+  def test_layout_links
     get root_path
-    # count is 1 for link in Home label
     assert_template 'pages/home'
+    # count is 1 for links in Home page to root
     assert_select "a[href=?]", root_path, count: 1
+    assert_select "a[href=?]", companies_path
     # Example of non-root path: Rails inserts help_path in place of ?
     # assert_select "a[href=?]", help_path
   end
+
 end
