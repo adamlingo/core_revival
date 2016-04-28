c72e3d6880bf6e9a2085c53f3f22e7d636ea1f7a
adamlingo <adamlingo@yahoo.com>
2016-04-28 08:23:47 -0500

    Comment out unknown syntax use

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,10 @@
 class Company < ActiveRecord::Base
+  # attr_accessor :name
+  # the above line blocks Admin save records for Co. name, look into
+
   # Company is parent of employees
   has_many :employees
+
+  validates_presence_of :name
 end
+  
