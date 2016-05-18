48e919e3262b076f1ddfa7cf6a26908615e036ae
adamlingo <adamlingo@yahoo.com>
2016-05-18 17:20:47 -0500

    Pages controller requires login, and Users controller added (blank)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 # use 'rails destroy controller Pages home' if replaced with other controller/specified dashboard
 class PagesController < ApplicationController
+  before_filter :authenticate_user!
+
   def home
   end
 
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+  # Empty new until decision on invitation vs. creation
+  def new
+  end
+
+  
+end
