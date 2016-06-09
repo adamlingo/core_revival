37ffcebcce3890886c407a7b244b4b8eb7cae862
adamlingo <adamlingo@yahoo.com>
2016-06-09 12:03:06 -0500

    Fixed syntax problem in migration

--- db/migrate/20160609170218_add_columns_to_benefit_profile.rb
+++ db/migrate/20160609170218_add_columns_to_benefit_profile.rb
@@ -2,6 +2,6 @@ class AddColumnsToBenefitProfile < ActiveRecord::Migration
   def change
     add_column :benefit_profiles, :provider, :string
     add_column :benefit_profiles, :provider_plan, :string
-    add_column :benefit_profiles, :type, :string
+    add_column :benefit_profiles, :benefit_type, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160609165001) do
+ActiveRecord::Schema.define(version: 20160609170218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 20160609165001) do
     t.datetime "updated_at",    null: false
     t.string   "provider"
     t.string   "provider_plan"
-    t.string   "type"
+    t.string   "benefit_type"
   end
 
   create_table "companies", force: :cascade do |t|
