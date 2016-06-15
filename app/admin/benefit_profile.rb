1e50105152a4369d52d6086e7c8fb88026e0c14f
adamlingo <adamlingo@yahoo.com>
2016-06-15 09:10:48 -0500

    Syntax fix + ben type options

--- app/admin/benefit_profile.rb
+++ app/admin/benefit_profile.rb
@@ -14,7 +14,8 @@ ActiveAdmin.register BenefitProfile do
       # EE must be attached to an existent Company
       f.input :company_id, as: :select, collection: Company.all.map{|c| ["#{c.name}", c.id]}
       f.input :provider, as: :select, :collection => [['BCBS'], ['Delta']]
-      f.input :provider_plan as: :select, :collection => [['Bronze']['Silver']]
+      f.input :provider_plan, as: :select, :collection => [['Bronze'],['Silver']]
+      f.input :benefit_type, as: :select, :collection => [['Medical'], ['Dental']]
     end
     f.actions
   end
