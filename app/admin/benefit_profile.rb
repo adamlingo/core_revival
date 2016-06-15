31abc49123d4b41fd8c1d6bd0d5e6b22b6d3d234
adamlingo <adamlingo@yahoo.com>
2016-06-15 09:05:36 -0500

    form to select details

--- app/admin/benefit_profile.rb
+++ app/admin/benefit_profile.rb
@@ -1,4 +1,21 @@
 ActiveAdmin.register BenefitProfile do
 
+ menu parent: "Companies"
 
+  # Edit fields to save
+  permit_params :name, :provider, :provider_plan, :benefit_type
+
+  # Index existent info could be here
+
+  # form
+  form do |f|
+    f.inputs "Benefit Details" do
+      # f.input :name, label: 'Provider Co. Name', :placeholder => "(i.e. BCBS)"
+      # EE must be attached to an existent Company
+      f.input :company_id, as: :select, collection: Company.all.map{|c| ["#{c.name}", c.id]}
+      f.input :provider, as: :select, :collection => [['BCBS'], ['Delta']]
+      f.input :provider_plan as: :select, :collection => [['Bronze']['Silver']]
+    end
+    f.actions
+  end
 end
