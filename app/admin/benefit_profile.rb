e8aa70c08a43bb6720a5cb64b14f112afe85eb43
adamlingo <adamlingo@yahoo.com>
2016-06-16 11:00:44 -0500

    Axe extra fields

--- app/admin/benefit_profile.rb
+++ app/admin/benefit_profile.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register BenefitProfile do
 
- menu parent: "Companies"
+ menu priority: 5
 
   # Edit fields to save
   permit_params :name, :company_id, :provider, :provider_plan, :benefit_type
@@ -10,7 +10,6 @@ ActiveAdmin.register BenefitProfile do
     selectable_column
     column :company_id
     id_column
-    # make EE list reflect showing name of Company here
     column :provider
     actions
   end
@@ -19,15 +18,11 @@ ActiveAdmin.register BenefitProfile do
   form do |f|
     f.inputs "Benefit Details" do
       # f.input :name, label: 'Provider Co. Name', :placeholder => "(i.e. BCBS)"
-      # EE must be attached to an existent Company
       f.input :company_id, as: :select, collection: Company.all.map{|c| ["#{c.name}", c.id]}
       f.input :provider, as: :select, :collection => [['BCBS'], ['Delta']]
       f.input :provider_plan, as: :select, :collection => [['Bronze'],['Silver']]
       f.input :benefit_type, as: :select, :collection => [['Medical'], ['Dental']]
-      #f.input :employee_category, as: :select, :collection => [['1'], ['2'], ['3'], ['4']]
-      #f.input :benefit_category, as: :select, :collection => [['SUB'], ['DEP'], ['SPS'], ['CH+1'], ['SPS+1']]
-      #f.input :employer_pay_type, as: :select, :collection => [['%'], ['$']]
-      #f.input :benefit_amount, :placeholder => "Benefit Amount"
+      
     end
     f.actions
   end
