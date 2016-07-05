2f4a7b6087791c62f13f8c058eee05402ef41eb7
adamlingo <adamlingo@yahoo.com>
2016-07-05 12:48:28 -0500

    Re-seeded db info since 'super_admin' no longer exists in records

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(email: 'admin@core.com', password: 'Core1204', password_confirmation: 'Core1204', super_admin: true)
+AdminUser.create!(email: 'admin@core.com', password: 'Core1204', password_confirmation: 'Core1204')
