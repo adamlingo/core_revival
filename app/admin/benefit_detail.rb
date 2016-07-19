54a46701f0e8a64459fd099b12264a50d871d933
adamlingo <adamlingo@yahoo.com>
2016-07-19 09:57:35 -0500

    Remove old category and amount

--- app/admin/benefit_detail.rb
+++ app/admin/benefit_detail.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register BenefitDetail do
 # belongs_to :benefit_profile
 belongs_to :employee
 
-permit_params :employee_category, :benefit_category, :benefit_amount, :benefit_method, :employee_id, :benefit_profile_id
+permit_params :employee_category, :benefit_method, :employee_id, :benefit_profile_id
 
 # index details to benefit_profile
 index do
@@ -26,8 +26,8 @@ index do
       f.input :employee_id, label: 'Name'
       #f.input :benefit_profile_id, as: :select, collection: BenefitProfile.where(company_id: Employee.find(:employee_id).company_id).map{|b| ["#{b.company_id} #{b.provider} #{b.provider_plan}", b.id]}
       f.input :employee_category, as: :select, :collection => [['1 Employee'], ['2 Manager'], ['3 Owner']]
-      f.input :benefit_category, as: :select, :collection => [['SUB'], ['DEP'], ['SPS'], ['CH+1'], ['SPS+1']]
-      f.input :benefit_method, as: :select, :collection => [['%'], ['$']]
+      # f.input :benefit_category, as: :select, :collection => [['SUB'], ['DEP'], ['SPS'], ['CH+1'], ['SPS+1']]
+      # f.input :benefit_method, as: :select, :collection => [['%'], ['$']]
       f.input :benefit_amount
 
     end
