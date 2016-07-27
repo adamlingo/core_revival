9af86a3ed484ea6a6023671565a2fa4c746709df
adamlingo <adamlingo@yahoo.com>
2016-07-27 13:03:12 -0500

    New migration for health invoice records

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160726220833) do
+ActiveRecord::Schema.define(version: 20160727174500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,6 +106,11 @@ ActiveRecord::Schema.define(version: 20160726220833) do
     t.string   "phone_number"
     t.string   "first_name"
     t.string   "last_name"
+  end
+
+  create_table "health_invoices", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
