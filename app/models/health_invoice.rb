377bce8a557f6bf271b561c85220898c1db1663c
adamlingo <adamlingo@yahoo.com>
2016-07-28 12:42:49 -0500

    All columns processed in csv import

--- app/models/health_invoice.rb
+++ app/models/health_invoice.rb
@@ -10,14 +10,22 @@ class HealthInvoice < ActiveRecord::Base
   end
 
   def self.import(file)
-    # Parse file name for Year/Month
-
+    # Parse file name for Year/Month>> string split by underscore [0]billing prof ID [1] month/year
+    # Check dups by parsing to billing profile ID and year/month 
     CSV.foreach(file.path, headers: true) do |row|
       health_invoice_hash = row.to_hash
-      puts health_invoice_hash
       # all columns in Health csv
       health_invoice = find_or_create_by!(account_number: health_invoice_hash['Account'],
-                                          sub_name: health_invoice_hash['Subscriber Name'])
+                                          billing_profile: health_invoice_hash['Billing Profile'],
+                                          category: health_invoice_hash['Category'],
+                                          product: health_invoice_hash['Product'],
+                                          health_sub_id: health_invoice_hash['Subscriber ID'],
+                                          sub_name: health_invoice_hash['Subscriber Name'],
+                                          tier: health_invoice_hash['Tier'],
+                                          change_reason: health_invoice_hash['Change Reason'],
+                                          retro_fee_adjustment: health_invoice_hash['Retro Fee Adjustment'],
+                                          current_charges: health_invoice_hash['Current Charges'],
+                                          total_charges: health_invoice_hash['Total Charges'])
       health_invoice.save!
     end
   end
