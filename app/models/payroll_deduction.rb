791a1c4764d272e5a35b9d971106e3c3e5f72dfd
adamlingo <adamlingo@yahoo.com>
2016-08-05 13:36:14 -0500

    Send up branch, to_csv method

--- app/models/payroll_deduction.rb
+++ app/models/payroll_deduction.rb
@@ -1,2 +1,13 @@
 class PayrollDeduction < ActiveRecord::Base
+  def self.to_csv(fields = column_names, options = {})
+    CSV.generate(options) do |csv|
+      csv << fields
+      all.each do |payroll_deduction|
+        csv << payroll_deduction.attributes.values_at(*fields)
+      end
+    end
+  end
+
+
+
 end
