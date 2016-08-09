8fd6e20a46a9b9a41608bfb6dbb867628823ad32
adamlingo <adamlingo@yahoo.com>
2016-08-09 14:52:25 -0500

    import method for payroll deductions csv in model

--- app/models/payroll_deduction.rb
+++ app/models/payroll_deduction.rb
@@ -8,6 +8,18 @@ class PayrollDeduction < ActiveRecord::Base
     end
   end
 
-
-
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      payroll_hash = row.to_hash
+      # all columns in Payroll Deduction csv
+      unless payroll_hash['Employee ID'].nil?
+        payroll_deduction = find_or_create_by!(pay_ee_id: payroll_hash['Employee ID'],
+                                            pay_sub_name: payroll_hash['Employee Name'],
+                                            pay_sub_id: payroll_hash['Subscriber #'],
+                                            pay_category: payroll_hash['Category#'],
+                                            deduction_amount: payroll_hash['Amount'])
+        payroll_deduction.save!
+      end
+    end
+  end
 end
