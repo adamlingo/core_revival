b387f3134e6d241987076f8f0a1f2aae48428d2b
adamlingo <adamlingo@yahoo.com>
2016-08-09 14:49:56 -0500

    Tests to check decimal positive and negative input

--- test/models/health_invoice_test.rb
+++ test/models/health_invoice_test.rb
@@ -8,4 +8,16 @@ class HealthInvoiceTest < ActiveSupport::TestCase
   def test_valid
     assert health_invoice.valid?
   end
+
+  def test_negative_input
+    expected = -24.55
+    actual = health_invoice.convert_to_decimal("(24.55)")
+    assert_equal expected.to_s, actual
+  end
+
+  def test_input
+    expected = 100.55
+    actual = health_invoice.convert_to_decimal(" $100.55 ")
+    assert_equal expected.to_s, actual
+  end
 end
