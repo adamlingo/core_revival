1df60adb379f74e5dae57c8ca2ef507810f6d432
adamlingo <adamlingo@yahoo.com>
2016-08-09 14:49:26 -0500

    Regex added to convert monetary fields

--- app/models/health_invoice.rb
+++ app/models/health_invoice.rb
@@ -16,6 +16,9 @@ class HealthInvoice < ActiveRecord::Base
     CSV.foreach(file.path, headers: true) do |row|
       health_invoice_hash = row.to_hash
       # all columns in Health csv
+      retro_fee_adjustment = HealthInvoice.convert_to_decimal(health_invoice_hash['RetroFeeAdjustment'])
+      current_charges = HealthInvoice.convert_to_decimal(health_invoice_hash['CurrentCharges'])
+      total_charges = HealthInvoice.convert_to_decimal(health_invoice_hash['TotalCharges'])
       health_invoice = find_or_create_by!(account_number: health_invoice_hash['Account'],
                                           billing_profile: health_invoice_hash['BillingProfile'],
                                           category: health_invoice_hash['Category'],
@@ -24,10 +27,20 @@ class HealthInvoice < ActiveRecord::Base
                                           sub_name: health_invoice_hash['Subscriber Name'],
                                           tier: health_invoice_hash['Tier'],
                                           change_reason: health_invoice_hash['Change Reason'],
-                                          retro_fee_adjustment: health_invoice_hash['RetroFee Adjustment'],
-                                          current_charges: health_invoice_hash['Current Charges'],
-                                          total_charges: health_invoice_hash['Total Charges'])
+                                          retro_fee_adjustment: retro_fee_adjustment,
+                                          current_charges: current_charges,
+                                          total_charges: total_charges)
       health_invoice.save!
     end
+  end
+
+  
+  def self.convert_to_decimal(input)
+    input.gsub(/\$|,/,'').gsub(/\(/,'-').gsub(/\)/,'').strip
+    # .strip remove space
+  end
+
+  def convert_to_date(file_name)
+
   end
 end
