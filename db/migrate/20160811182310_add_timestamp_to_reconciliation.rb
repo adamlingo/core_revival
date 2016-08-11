a091ea399b46cbbe6e9f737eb076b5e235c5f315
adamlingo <adamlingo@yahoo.com>
2016-08-11 13:47:13 -0500

    add timestamp manually

--- db/migrate/20160811182310_add_timestamp_to_reconciliation.rb
+++ db/migrate/20160811182310_add_timestamp_to_reconciliation.rb
@@ -1,4 +1,6 @@
 class AddTimestampToReconciliation < ActiveRecord::Migration
   def change
+    add_column :reconciliations, :created_at, :datetime
+    add_column :reconciliations, :updated_at, :datetime
   end
 end
