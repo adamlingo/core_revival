33603a5b3f71cbcdcd81bda052cd9ef37e25a2d3
Christian Brim <cbrim@myemployeesolution.com>
2016-08-18 01:45:43 +0000

    Add semi-monthly, weekly, bi-weekly

---
+++ PayDates.rb
@@ -0,0 +1,103 @@
+require 'date'
+
+class PayrollDate
+  def self.generate_monthly(company_id, start_date, num_periods)
+    puts 'generate monthly payroll dates'
+    puts "company_id: #{company_id}; start_date: #{start_date}; num_periods: #{num_periods}"
+
+    # create a real Date from our string start_date
+    date = Date.parse(start_date)
+    puts "date: #{date}"
+
+    # create a PayrollDate and save?
+    # PayrollDate.create!(company_id: company_id,
+    # 					date: date,
+    # 					period: 1)
+
+    (2..num_periods).each do |period|
+      date = date >> 1
+      puts "loop date: #{date}"
+
+    # PayrollDate.create!(company_id: company_id,
+    # 					date: date,
+    # 					period: period)
+    end
+  end
+  
+    def self.generate_weekly(company_id, start_date, num_periods)
+      puts 'generate weekly payroll dates'
+          
+        date = Date.parse(start_date)
+          
+        (2..num_periods).each do |period|
+          date = date + 7
+          puts "loop date: #{date}"
+        end
+    end
+    
+    def self.generate_bi_weekly(company_id, start_date, num_periods)
+      puts 'generate bi-weekly payroll dates'
+          
+       date = Date.parse(start_date)
+          
+        (2..num_periods).each do |period|
+          date = date + 14
+          puts "loop date: #{date}"
+        end
+    end
+    
+    def self.generate_semi_monthly(company_id, start_date, num_periods)
+      puts 'generate semi-monthly payroll dates'
+          
+        date = Date.parse(start_date)
+          
+        (2..num_periods).each do |period|
+          case date.day
+          
+            when 1
+              date = Date.new(date.year,date.month,16)
+              puts "loop date: #{date}"
+            when 15
+              date = Date.new(date.year,date.month,-1)
+              puts "loop date: #{date}"
+            when 16
+              date = Date.new(date.year,date.month,1)
+              puts "loop date: #{date}"
+            else
+              date = Date.new(date.year,date.month >> 1, 1)
+              puts "loop date: #{date}"
+          # how to increment by half month?  Most likely 1st and 16th OR 15th and end of month
+          # this would not work if date was other than thsoe four
+          end
+        end
+    end
+end
+
+PayrollDate.generate_semi_monthly(1, '2016-8-15', 5)
+
+PayrollDate.generate_semi_monthly(2, '2016-8-1', 5)
+
+PayrollDate.generate_semi_monthly(3, '2016-8-16', 5)
+
+PayrollDate.generate_semi_monthly(4, '2016-8-31', 5)
+
+# PayrollDate.generate_bi_weekly(1, '2016-8-15', 5)
+
+# puts "------------------"
+
+# PayrollDate.generate_bi_weekly(2, '2016-8-1', 5)
+
+# puts "------------------"
+
+# PayrollDate.generate_bi_weekly(3, '2016-8-31', 5)
+
+
+# PayrollDate.generate_monthly(1, '2016-07-01', 3)
+
+# puts "----------------"
+
+# PayrollDate.generate_monthly(2, '2016-08-15', 6)
+
+# puts "----------------"
+
+# PayrollDate.generate_monthly(3, '2016-09-23', 6)
--- app/models/payroll_period.rb
+++ app/models/payroll_period.rb
@@ -18,18 +18,17 @@ class CompanyPayrollDate < ActiveRecord::Base
     end
   end
 
+
+
   private
    
-
-    
-
     def self.generate_monthly(company_id, start_date, num_periods)
       puts 'generate monthly payroll dates'
       puts "company_id: #{company_id}; start_date: #{start_date}; num_periods: #{num_periods}"
     
       # create a real Date from our string start_date
     
-       date = Date.parse(start_date)
+       date = Date.parse(start_date)  # is there a refactor that would keep from repeating?
     
           
          (2..num_periods).each do |period|
@@ -37,36 +36,45 @@ class CompanyPayrollDate < ActiveRecord::Base
          end
     end
 
-
-  
-    
-    def self.generate_semi_monthly(company_id, start_date, num_periods, pay_frequency)
+    def self.generate_semi_monthly(company_id, start_date, num_periods)
       puts 'generate semi-monthly payroll dates'
           
         date = Date.parse(start_date)
           
         (2..num_periods).each do |period|
-          date = date 
+          case date.day
+          
+            when 1
+              date = Date.new(date.year,date.month,16)
+            when 15
+              date = Date.new(date.year,date.month,-1)
+            when 16
+              date = Date.new(date.year,date.month,1)
+            else
+              date = Date.new(date.year,date.month >> 1, 1)
+          # how to increment by half month?  Most likely 1st and 16th OR 15th and end of month
+          # this would not work if date was other than thsoe four
+          end
         end
     end
     
-    def self.generate_weekly(company_id, start_date, num_periods, pay_frequency)
+    def self.generate_weekly(company_id, start_date, num_periods)
       puts 'generate weekly payroll dates'
           
         date = Date.parse(start_date)
           
-        (2.num_periods).each do |period|
-          date = date + 7.days
+        (2..num_periods).each do |period|
+          date = date + 7
         end
     end
     
-    def self.generate_bi_weekly(company_id, start_date, num_periods, pay_frequency)
+    def self.generate_bi_weekly(company_id, start_date, num_periods)
       puts 'generate bi-weekly payroll dates'
           
        date = Date.parse(start_date)
           
-        (2.num_periods).each do |period|
-          date = date + 14.days
+        (2..num_periods).each do |period|
+          date = date + 14
         end
     end
 end
