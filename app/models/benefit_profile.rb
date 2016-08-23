782107a8dd23a76fc5814a20eb61277c8deb42a7
adamlingo <adamlingo@yahoo.com>
2016-08-23 10:30:17 -0500

    fix associations of benefit detail

--- app/models/benefit_profile.rb
+++ app/models/benefit_profile.rb
@@ -1,7 +1,5 @@
 class BenefitProfile < ActiveRecord::Base
   belongs_to :company
   has_many :benefit_details
-  has_many :employees, through: :benefit_details
-  
-  
+  has_many :employees, through: :company
 end
