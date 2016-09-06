abdb984c853bc42b2a2baba89a30b8088e39f4d6
adamlingo <adamlingo@yahoo.com>
2016-09-06 18:33:00 -0500

    Move strong params to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,11 @@ class UsersController < ApplicationController
   def new
   end
 
-  
+  private
+    # Use callbacks to share common setup or constraints between actions.
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation, :admin, :employee, :manager)
+    end
 end
