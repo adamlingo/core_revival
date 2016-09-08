8e4783ad24bcc43c97354335251c735688343b6f
Christian Brim <cbrim@myemployeesolution.com>
2016-09-08 00:35:45 +0000

    refactor rake task api_pto_call

--- app/models/timework.rb
+++ app/models/timework.rb
@@ -4,7 +4,7 @@ class Timework < ActiveRecord::Base
     belongs_to :company
 
     def self.build_encoded_uri(user_id, password)
-        api_url = 'http://www.payrollservers.us/pg/api12.asmx'
+        api_url = 'http://www.swipeclock.com/pg/api12.asmx'
         request_url = "#{api_url}/CreateSessionSelectClient"
         query_params = [
             ['login', user_id],
@@ -18,6 +18,28 @@ class Timework < ActiveRecord::Base
         uri
     end
     
+    
+    def self.pto_report
+        
+        session_id = Timework.createSession('fiducialok','Thunder2016')
+            puts session_id
+            api_url = 'https://www.payrollservers.us/pg/api12.asmx'
+            request_url = "#{api_url}/GetActivityFile?sessionID=#{session_id}"
+            query_params = [
+                ['BeginDate', '2016-8-14'],
+                ['EndDate', '2016-8-27'],
+                ['FormatName', 'swipeclockpto'],
+                ['OptionalLaborMapping','']
+                ]
+                uri = URI.parse(request_url)
+                uri.query = URI.encode_www_form(query_params)
+                uri
+                req = Net::HTTP::Get.new(uri)
+                res = Net::HTTP.start(uri.host, uri.port) {|http|
+                        http.request(req)
+                }
+    end
+            
     def self.createSession(user_id, password)
 
         uri = build_encoded_uri(user_id, password)
@@ -27,6 +49,6 @@ class Timework < ActiveRecord::Base
         }
         h = Hash.from_xml(res.body)
         h['CreateSessionResult']['SessionID']
-        
+       
     end
 end
