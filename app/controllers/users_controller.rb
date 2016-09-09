d686d32282f95a7799d410cb0b4d29ece88d278c
adamlingo <adamlingo@yahoo.com>
2016-09-09 16:35:51 -0500

    Remove need for admin/ee boolean params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,6 @@ class UsersController < ApplicationController
     end
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :admin, :employee, :manager)
+      params.require(:user).permit(:email, :password, :password_confirmation, :manager)
     end
 end
