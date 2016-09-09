d2764effc8763116deb4e336adc8f6cf063b6e5c
Rick Yoesting <rick.yoesting@gmail.com>
2016-09-08 20:34:21 -0400

    a new function to accept the clientID too

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,4 +2,13 @@ class Employee < ActiveRecord::Base
   # child of Company
   belongs_to :company
   has_many :benefit_profiles, through: :company
+
+  def self.save_pto(company_id, raw_data)
+  	puts "company_id: #{company_id}\nraw_data:\n#{raw_data}"
+
+  	# do the work...
+  	# parse the raw_data
+  	# look up the Employee
+  	# add/update the PTO balance for that employee
+  end
 end
--- app/models/timework.rb
+++ app/models/timework.rb
@@ -5,18 +5,57 @@ class Timework < ActiveRecord::Base
 
     API_URL = 'https://www.swipeclock.com/pg/api12.asmx'.freeze
 
-    def self.build_encoded_uri(user_id, password)
+    def self.build_encoded_uri(user_id, password, client_id)
         request_url = "#{API_URL}/CreateSessionSelectClient"
         query_params = [
             ['login', user_id],
             ['password', password],
             ['secondFactor', ''],
             ['matchfield', 'ClientName'],
-            ['ClientID', 'Green Bambino']
+            ['ClientID', client_id]
             ]
         uri = URI.parse(request_url)
         uri.query = URI.encode_www_form(query_params)
         uri
+    end
+
+    def self.createSession(user_id, password, client_id)
+        uri = build_encoded_uri(user_id, password, client_id)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+
+        req = Net::HTTP::Get.new(uri)
+        res = http.request(req)
+
+        h = Hash.from_xml(res.body)
+        h['CreateSessionResult']['SessionID']       
+    end
+
+    def self.pto_report_by_client(user_id, password, client_id)
+        session_id = Timework.createSession(user_id, password, client_id)
+        puts session_id
+
+        request_url = "#{API_URL}/GetActivityFile"
+        query_params = [
+            ['sessionID', session_id],
+            ['BeginDate', '2016-8-14'],
+            ['EndDate', '2016-8-27'],
+            ['FormatName', 'swipeclockpto'],
+            ['OptionalLaborMapping','']
+        ]
+
+        uri = URI.parse(request_url)
+        uri.query = URI.encode_www_form(query_params)
+
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+
+        req = Net::HTTP::Get.new(uri)
+        res = http.request(req)
+
+        h = Hash.from_xml(res.body)
+        # return the CSV data
+        h['GetActivityFileApiResult']['FormatText']       
     end
 
     def self.pto_report
@@ -41,18 +80,9 @@ class Timework < ActiveRecord::Base
         req = Net::HTTP::Get.new(uri)
         res = http.request(req)
 
-        puts res.body
-    end
-            
-    def self.createSession(user_id, password)
-        uri = build_encoded_uri(user_id, password)
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
-
-        req = Net::HTTP::Get.new(uri)
-        res = http.request(req)
-
         h = Hash.from_xml(res.body)
-        h['CreateSessionResult']['SessionID']       
+        # return the CSV data
+        h['GetActivityFileApiResult']['FormatText']       
     end
+
 end
