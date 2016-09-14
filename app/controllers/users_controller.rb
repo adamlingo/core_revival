f21514557c45e624f59de18f86ca5b452d8cba9b
adamlingo <adamlingo@yahoo.com>
2016-09-14 18:38:38 -0500

    Re-add users controller with index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,9 @@
 class UsersController < ApplicationController
   # Empty new until decision on invitation vs. creation
   def index
+    @users = User.all
+    # used in future for showing all users
+    # controllers/users/ is where devise controllers exist
   end
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    # need to add redirect for user type
-
-    # creating a new user creates a new session
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to '/home'
-    else
-      redirect_to '/users/signup'
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :manager)
-    end
 end
