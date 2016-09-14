799657f7e4d40bf519e42303b6714081deaf8b85
adamlingo <adamlingo@yahoo.com>
2016-09-14 18:37:31 -0500

    Explicit routes for sessions/registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   
   # Devise login is home page based on pages#home before_filter
-  devise_scope :user do
-  end
 
   # Root to pages/home (requires login)
   root 'pages#home'
@@ -14,23 +12,9 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
 
   # Devise user routes
-  devise_for :users
-  # All included routes for Devise (rake routes):
-  #      new_user_session GET    /users/sign_in(.:format)               devise/sessions#new
-  #          user_session POST   /users/sign_in(.:format)               devise/sessions#create
-  #  destroy_user_session DELETE /users/sign_out(.:format)              devise/sessions#destroy
-  #         user_password POST   /users/password(.:format)              devise/passwords#create
-  #     new_user_password GET    /users/password/new(.:format)          devise/passwords#new
-  #    edit_user_password GET    /users/password/edit(.:format)         devise/passwords#edit
-  #                       PATCH  /users/password(.:format)              devise/passwords#update
-  #                       PUT    /users/password(.:format)              devise/passwords#update
-  # cancel_user_registration GET    /users/cancel(.:format)             devise/registrations#cancel
-  #     user_registration POST   /users(.:format)                       devise/registrations#create
-  # new_user_registration GET    /users/sign_up(.:format)               devise/registrations#new
-  #edit_user_registration GET    /users/edit(.:format)                  devise/registrations#edit
-  #                       PATCH  /users(.:format)                       devise/registrations#update
-  #                       PUT    /users(.:format)                       devise/registrations#update
-  #                       DELETE /users(.:format)                       devise/registrations#destroy
+  devise_for :users, controllers: { sessions: "users/sessions",
+                                    registrations: "users/registrations"}
+  
 
   # make Employees editable records that belong to companies by id
   resources :companies do
