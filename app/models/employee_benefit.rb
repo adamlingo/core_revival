b3e90a2994b3caaecbd51fa44ea0c12e90660615
Christian Brim <cbrim@myemployeesolution.com>
2016-09-22 01:14:49 +0000

    Initial view for employee_benefit

--- app/models/employee_benefit.rb
+++ app/models/employee_benefit.rb
@@ -1,11 +1,12 @@
 class EmployeeBenefit < ActiveRecord::Base
     belongs_to :employee
     
-    def self.import
-        CSV.foreach(raw_data, headers: true) do |row|
+    def self.import(raw_data)
+        CSV.parse(raw_data, headers: true) do |row|
           pto_hash = row.to_hash
           pto_balance = find_or_create_by!(ee_id: pto_hash['EE Code'],
-                                         end_balance: pto_hash['Ending Balance'])
+                                         pto_balance: pto_hash['Ending Balance'])
           pto_balance.save!
+        end
     end
 end
