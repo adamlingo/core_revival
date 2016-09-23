8527150138509246e9324276cf4b9ec7ed395ccc
adamlingo <adamlingo@yahoo.com>
2016-09-23 16:49:55 -0500

    User model links company id (to be used to index only present co's ee's)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  def company_id
+    e = Employee.find_by(user_id: id)
+    e.company_id
+  end
+
 end
