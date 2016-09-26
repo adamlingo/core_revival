ef6d7d1157af53f518dff84537b086998e9a7157
adamlingo <adamlingo@yahoo.com>
2016-09-26 18:25:38 -0500

    User model accesses current user as employee

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,25 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  # find company ID for current user.
   def company_id
     e = Employee.find_by(user_id: id)
     e.company_id
+  end
+
+  def company_name
+    e = Employee.find_by(user_id: id)
+    e.company.name
+  end
+
+  def first_name
+    e = Employee.find_by(user_id: id)
+    e.first_name
+  end
+
+  def last_name
+    e = Employee.find_by(user_id: id)
+    e.last_name
   end
 
 end
