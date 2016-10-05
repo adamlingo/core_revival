9d498828f47c3529c87d4e64611f662972fed2d8
adamlingo <adamlingo@yahoo.com>
2016-10-05 13:59:22 -0500

    Remove old docu.

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,11 +1,7 @@
 class Company < ActiveRecord::Base
-  # attr_accessor :name
-  # the above line blocks Admin save records for Co. name, look into
-
-  # Company is parent of employees
+  # Company is parent of employees, ben. profiles
   has_many :employees
   has_many :benefit_profiles
-  # has_many :benefit_details
  
   validates_presence_of :name
 end
