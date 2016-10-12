f819a0f40de4a9071f62c77816e569bc09478a06
adamlingo <adamlingo@yahoo.com>
2016-10-12 14:29:13 -0500

    config environments for dev and prod to user mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,18 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  # ADD defined default environment variables for URL
+  # ActionMailer setup for Mailgun smtp
   config.action_mailer.default_url_options = { host: 'http://localhost:3000' }
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    :port =>           '587',
+    :address =>        'smtp.mailgun.org',
+    :user_name =>      ENV['MAILGUN_USERNAME'],
+    :password =>       ENV['MAILGUN_PASSWORD'],
+    :domain =>         'core-redux.herokuapp.com',
+    :authentication => :plain
+  }
 end
