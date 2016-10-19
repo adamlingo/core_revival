7658d66063d69f52edc173f7317fd91f08e30cc3
Rick Yoesting <rick.yoesting@gmail.com>
2016-10-18 20:01:43 -0400

    JWT FTW

--- app/services/sso.rb
+++ app/services/sso.rb
@@ -6,32 +6,36 @@ class SSO < ActiveRecord::Base
 # create originating token
     def self.sso #(employee)
     
-        # header = [
-        #   ["typ"=>"JWT", "alg"=>"HS256"] # header
-        #   ]
+        # header = {
+        #   'typ' => 'JWT',
+        #   'alg' => 'HS256'
+        # }
+
         t = Time.now + 60
       
         # emp_id = EmployeeAdditionalLogin.find_by(employee_id: employee.id)
-        payload = [
-          ['iss'=>539, # payload
-          "exp"=>Time.now.to_i, # need future time not more than 5 minutes
-          "user"=> ["type"=> "empcode", "id"=>0156],
-          "site"=> 53466,
-          "iat"=> t.to_i,
-          "product"=>"twpemp"
-          ]
-          ]
+        payload = {
+          'iss' => 539,
+          'exp' => t.to_i,
+          'user' => {
+            'type' => 'empcode',
+            'id' => '0156'
+          },
+          'site' => 53466,
+          'iat' => t.to_i,
+          'product' => 'twpemp'          
+        }
           
+        puts "payload: #{payload}"
         hmac_secret = 'XMhgNMqD550tqqDhj1Ic5cDvghhQbjNaUd1xdwQ1CXNDspCmf2ljIpNneGfHfzOz'
         
         token = JWT.encode payload, hmac_secret, 'HS256'
-        
-        
-        puts token
+                
+        puts "token: #{token}"
         
         decoded_token = JWT.decode token, hmac_secret, true, { :algorithm => 'HS256' }
        
-        puts decoded_token
+        puts "decoded_token: #{decoded_token}"
     end
 
 
