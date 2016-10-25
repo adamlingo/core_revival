f4e9b87ddb9ed92d39d3c142593e600a3ab2c248
adamlingo <adamlingo@yahoo.com>
2016-10-25 16:21:20 -0500

    folder scaffolded controller/helper/model

---
+++ app/models/folder.rb
@@ -0,0 +1,10 @@
+class Folder < ActiveRecord::Base
+  validates_presence_of :title
+
+  attachment :image, type: :image
+
+  has_many :documents, dependent: :destroy
+
+  accepts_attachments_for :documents
+
+end
