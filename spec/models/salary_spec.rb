2c849330333c011a92a75f4bfaeeab47870c4099
Christian Brim <cbrim@myemployeesolution.com>
2016-11-05 18:01:16 +0000

    rspec salary model tests

--- spec/models/salary_spec.rb
+++ spec/models/salary_spec.rb
@@ -1,4 +1,44 @@
-# require 'rails_helper'
+require 'rails_helper'
+
+describe Salary, :type => :model do
+    
+    it "is invalid when saved" do
+        salary = Salary.new
+        expect(salary.valid?).to eq(false)
+    end
+        
+    
+    it "is valid when saved" do
+        
+         salary = Salary.new(employee_id: 1, start_date: '2016-10-31', pay_type: 'test', rate: 40)
+         
+         expect(salary.valid?).to be(true)
+    end
+    
+    it "has a start_date" do
+    
+        salary = Salary.new(employee_id: 1, pay_type: 'test', rate: 40)
+         
+        expect(salary.valid?).to be(false)
+        
+    end
+    
+    it "has a rate" do
+        
+        salary = Salary.new(employee_id: 1, start_date: '2016-10-31', pay_type: 'test')
+         
+        expect(salary.valid?).to be(false)
+        
+    end
+    
+    it "has a pay_type" do
+                 
+        salary = Salary.new(employee_id: 1, start_date: '2016-10-31', rate: 40)
+        
+        expect(salary.valid?).to be(false)
+    end
+    
+end
 
 
 # describe Salary, :type => :model do
