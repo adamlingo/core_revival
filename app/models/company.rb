57c59937cf472ff3afb2f7a8a84577f419b5534a
adamlingo <adamlingo@yahoo.com>
2016-11-10 17:23:37 -0600

    Try to fix associations for payroll data (needs more)

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,7 +1,6 @@
 class Company < ActiveRecord::Base
   # Company is parent of employees, ben. profiles
   has_many :employees
-  has_many :payroll_records
   has_many :benefit_profiles
   has_many :company_folders
  
