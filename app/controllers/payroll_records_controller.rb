b7d525b1e1ae62a30ed912d124e30a90b12b0141
adamlingo <adamlingo@yahoo.com>
2016-11-16 12:13:28 -0600

    Remove default actions and test a new create action

--- app/controllers/payroll_records_controller.rb
+++ app/controllers/payroll_records_controller.rb
@@ -24,37 +24,18 @@ class PayrollRecordsController < ApplicationController
   end
 
   def create
-    @payroll_record = PayrollRecord.new(payroll_record_params)
+    puts "*******************"
+    puts params['24-reg_hours']
 
-    respond_to do |format|
-      if @payroll_record.save
-        format.html { redirect_to @payroll_record, notice: 'Payroll record was successfully created.' }
-        format.json { render :show, status: :created, location: @payroll_record }
-      else
-        format.html { render :new }
-        format.json { render json: @payroll_record.errors, status: :unprocessable_entity }
-      end
-    end
+    redirect_to company_payroll_records_path
   end
 
   def update
-    respond_to do |format|
-      if @payroll_record.update(payroll_record_params)
-        format.html { redirect_to @payroll_record, notice: 'Payroll record was successfully updated.' }
-        format.json { render :show, status: :ok, location: @payroll_record }
-      else
-        format.html { render :edit }
-        format.json { render json: @payroll_record.errors, status: :unprocessable_entity }
-      end
-    end
+    
   end
 
   def destroy
-    @payroll_record.destroy
-    respond_to do |format|
-      format.html { redirect_to payroll_records_url, notice: 'Payroll record was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    
   end
 
   private
@@ -63,12 +44,12 @@ class PayrollRecordsController < ApplicationController
       Company.find(params[:company_id].to_i)
     end
     # Use callbacks to share common setup or constraints between actions.
-    def set_payroll_record
-      @payroll_record = PayrollRecord.find(params[:id])
-    end
+    # def set_payroll_record
+    #   @payroll_record = PayrollRecord.find(params[:id])
+    # end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def payroll_record_params
-      params.fetch(:payroll_record, {})
-    end
+    # def payroll_record_params
+    #   params.fetch(:payroll_record, {})
+    # end
 end
