e2a2b334823818b58ad95d517986e4591b828089
Christian Brim <cbrim@myemployeesolution.com>
2016-11-26 01:16:32 +0000

    import method for benefit_rate. not complete

--- app/models/benefit_profile.rb
+++ app/models/benefit_profile.rb
@@ -3,4 +3,7 @@ class BenefitProfile < ActiveRecord::Base
   has_many :benefit_details
   has_many :employees, through: :company
   has_one :benefit_accept
+  
+  validates_presence_of :company_id
+  
 end
--- app/models/benefit_rate.rb
+++ app/models/benefit_rate.rb
@@ -10,7 +10,17 @@ class BenefitRate < ActiveRecord::Base
     def self.compute_rate
     end
     
-    def self.import
+    def self.import(benefit_detail_id, file_path)
+       # file_path = '/home/ubuntu/workspace/core_redux/test/fixtures/BCBS G712PFR.csv'
+        CSV.foreach(file_path, headers: true) do |row|
+           import_hash = row.to_hash
+           unless import_hash['Age'].nil?
+                import = find_or_create_by!(age: import_hash['Age'],
+                                            benefit_detail_id: benefit_detail_id,
+                                            rate: import_hash['Rate'])
+            import.save!
+           end
+        end
     end
     
 end
