6937fd952618462bf2d265379bffd06ee5f16416
Christian Brim <cbrim@myemployeesolution.com>
2016-12-17 16:28:48 +0000

    add names to dependents

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161217155104) do
 
+ActiveRecord::Schema.define(version: 20161217162725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -146,6 +146,8 @@ ActiveRecord::Schema.define(version: 20161217155104) do
     t.date     "date_of_birth"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   create_table "documents", force: :cascade do |t|
