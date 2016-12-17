c2e0881e70d88edf7ad163639163815fda478f3c
Christian Brim <cbrim@myemployeesolution.com>
2016-12-17 16:11:41 +0000

    forgot to migrate

--- db/migrate/20161217155958_dependents_to_benefit_profile.rb
+++ db/migrate/20161217155958_dependents_to_benefit_profile.rb
@@ -1,4 +1,6 @@
 class DependentsToBenefitProfile < ActiveRecord::Migration
   def change
+    add_column :benefit_profiles, :spouse_eligible, :boolean
+    add_column :benefit_profiles, :child_eligible, :boolean
   end
 end
