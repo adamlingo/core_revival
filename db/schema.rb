3173c8d5a554ddb1cc82cb42f2918f9cce81a0a8
Christian Brim <cbrim@myemployeesolution.com>
2016-12-17 16:18:42 +0000

    Add dependent model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161217155958) do
+ActiveRecord::Schema.define(version: 20161217161357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -130,6 +130,14 @@ ActiveRecord::Schema.define(version: 20161217155958) do
     t.text     "address"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "dependents", force: :cascade do |t|
+    t.integer  "employee_id"
+    t.string   "relationship"
+    t.date     "date_of_birth"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "documents", force: :cascade do |t|
