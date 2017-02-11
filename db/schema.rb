76f243f533c263cad65646f8ee4b9bd59267b8b6
Christian Brim <cbrim@myemployeesolution.com>
2017-02-11 23:01:24 +0000

    add ecnrypted ssn

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170201162040) do
-=======
 ActiveRecord::Schema.define(version: 20170211222547) do
->>>>>>> 6e5d7bf... add ecnrypted ssn
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
